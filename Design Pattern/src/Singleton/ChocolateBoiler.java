5e7f69e3e88d55b095eef83f2c5925b9daceec7f
eggtrt <eggtrt@gmail.com>
2024-02-17 23:08:46 +0900

    중복 생성 방지 -1

--- Design Pattern/src/Singleton/ChocolateBoiler.java
+++ Design Pattern/src/Singleton/ChocolateBoiler.java
@@ -10,7 +10,7 @@ public class ChocolateBoiler {
         boiled = false;
     }
 
-    public static ChocolateBoiler getInstance() {
+    public static synchronized ChocolateBoiler getInstance() {
         if (uniqueBoiler == null) {
             uniqueBoiler = new ChocolateBoiler();
         }

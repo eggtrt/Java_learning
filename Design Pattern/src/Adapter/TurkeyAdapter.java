fdf0967896b6d2469d670cd83daa84cf82dd18f7
eggtrt <eggtrt@gmail.com>
2024-02-20 23:07:33 +0900

    Adapter base

---
+++ Design Pattern/src/Adapter/TurkeyAdapter.java
@@ -0,0 +1,16 @@
+package Adapter;
+
+public class TurkeyAdapter implements Duck {
+    Turkey turkey;
+    public TurkeyAdapter(Turkey turkey) {
+        this.turkey = turkey;
+    }
+
+    public void quack() {
+        turkey.gobble();
+    }
+
+    public void fly() {
+        turkey.fly();
+    }
+}

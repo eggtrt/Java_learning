47d488bfa636e30d542902106a991f95a91e6b40
eggtrt <eggtrt@gmail.com>
2024-02-26 22:54:49 +0900

    basic add

---
+++ Basic/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!"); // ""은 string type
+        System.out.println('H'); // '' 은 character type
+
+        double a = 1.1
+    }
+}
